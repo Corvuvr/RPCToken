b42148ef09966fcaf5cb5cea630bd9bf479bef53
Corvuvr <d.podgornyy@inbox.ru>
2024-06-07 23:32:51 +0300

    Remove unnecessary directory

--- migrations/RPCMigrate.js
+++
@@ -1,5 +0,0 @@
-const RPCToken = artifacts.require("RPCToken");
-
-module.exports = function (deployer) {
-  deployer.deploy(RPCToken, 1000000); // Убедитесь, что здесь указан правильный параметр конструктора
-};
